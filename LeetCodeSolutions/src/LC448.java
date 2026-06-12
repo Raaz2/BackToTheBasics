7b67e259bf00812d227ee891391fd034fb088f02
Rajeev Ranjan Mishra <rajeevranjanmishra4339@gmail.com>
2026-06-12 23:58:49 +0530

    LC 448

---
+++ LeetCodeSolutions/src/LC448.java
@@ -0,0 +1,36 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+//https://leetcode.com/problems/find-all-numbers-disappeared-in-an-array/
+public class LC448 {
+
+    public static void main(String[] args) {
+        int[] arr = {4,3,2,7,8,2,3,1};
+        System.out.println(findDisappearedNumbers(arr));
+    }
+
+    public static List<Integer> findDisappearedNumbers(int[] arr) {
+        for (int i = 0; i < arr.length;) {
+            int correct = arr[i] - 1;
+
+            if (arr[correct] != arr[i]) {
+                int temp = arr[correct];
+                arr[correct] = arr[i];
+                arr[i] = temp;
+            }
+            else {
+                i++;
+            }
+        }
+
+        List<Integer> list = new ArrayList<>();
+        for (int i = 0, j = 1; i < arr.length; i++, j++) {
+            if (arr[i] != j) {
+                list.add(j);
+            }
+        }
+
+
+        return list;
+    }
+}
