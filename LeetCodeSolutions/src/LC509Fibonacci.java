886aeb683f2fd3797e6bacee135044c616972e06
Rajeev Ranjan Mishra <rajeevranjanmishra4339@gmail.com>
2026-06-20 23:56:18 +0530

    LC509Fib

---
+++ LeetCodeSolutions/src/LC509Fibonacci.java
@@ -0,0 +1,13 @@
+public class LC509Fibonacci {
+    public static void main(String[] args) {
+        int n = 5;
+        System.out.println(fib(n));
+    }
+
+    public static int fib(int n) {
+        if (n < 0) return -1; // not available
+        if (n == 1 || n == 0) return n;
+        return fib(n - 1) + fib(n - 2);
+    }
+
+}
