2b3ad655ebdc01c6ee00609c2e10b5dca7ae1a86
Rajeev Ranjan Mishra <rajeevranjanmishra4339@gmail.com>
2026-06-21 00:22:03 +0530

    RecursiveBSLC704

---
+++ LeetCodeSolutions/src/LC704RecursiveBinarySearch.java
@@ -0,0 +1,23 @@
+public class LC704RecursiveBinarySearch {
+    public static void main(String[] args) {
+        int[] arr = {-1,0,3,5,9,12};
+        System.out.println(binarySearch(arr, 0, arr.length - 1, 9));
+    }
+
+    public static int binarySearch(int[] arr, int start, int end, int target) {
+
+        if (start > end) return -1;
+
+        int mid = start + (end - start) / 2;
+
+        if (arr[mid] == target) {
+            return mid;
+        }
+        else if (arr[mid] > target) {
+            return binarySearch(arr, start, mid - 1, target);
+        }
+        else {
+            return binarySearch(arr, mid + 1, end, target);
+        }
+    }
+}
