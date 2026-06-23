9149b02276a4204e7daba7f00d53a7a05ed39301
Rajeev Ranjan Mishra <rajeevranjanmishra4339@gmail.com>
2026-06-23 23:43:20 +0530

    code

--- LeetCodeSolutions/src/LC231PowerOfTwo.java
+++ LeetCodeSolutions/src/LC231PowerOfTwo.java
@@ -2,7 +2,7 @@ public class LC231PowerOfTwo {
     public static void main(String[] args) {
         System.out.println(isPowerOfTwoBitwise(17));
         System.out.println(isPowerOfTwoIteration(36));
-        System.out.println(isPowerOfTwoRecursive(36));
+        System.out.println(isPowerOfTwoRecursive(128));
     }
     public static boolean isPowerOfTwoBitwise(int n) {
         if (n <= 0) return false;
@@ -16,10 +16,9 @@ public class LC231PowerOfTwo {
         return n == 1;
     }
     public static boolean isPowerOfTwoRecursive(int n) {
-        if (n <= 0) return false;
-        while (n % 2 == 0) {
-            n /= 2;
-        }
-        return n == 1;
+        if (n == 1) return true;
+        if (n <= 0 || (n % 2 != 0)) return false;
+
+        return isPowerOfTwoRecursive(n / 2);
     }
 }
