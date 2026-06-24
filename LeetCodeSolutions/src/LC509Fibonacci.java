c48a311d67bbf3cbc4062a7fbcee0d999ba364e3
Rajeev Ranjan Mishra <rajeevranjanmishra4339@gmail.com>
2026-06-24 23:47:44 +0530

    FibUsingBinetForm

--- LeetCodeSolutions/src/LC509Fibonacci.java
+++ LeetCodeSolutions/src/LC509Fibonacci.java
@@ -1,7 +1,8 @@
 public class LC509Fibonacci {
     public static void main(String[] args) {
         int n = 5;
-        System.out.println(fib(n));
+//        System.out.println(fib(n));
+        System.out.println(fib1(50));
     }
 
     public static int fib(int n) {
@@ -10,4 +11,8 @@ public class LC509Fibonacci {
         return fib(n - 1) + fib(n - 2);
     }
 
+    public static long fib1(int n) {
+        double phi = (1 + Math.sqrt(5)) / 2;
+        return (long) Math.round(Math.pow(phi, n) / Math.sqrt(5));
+    }
 }
