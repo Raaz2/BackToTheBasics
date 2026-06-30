dd66f17dc0c8615bd7c531efa0f3e18683feedde
Rajeev Ranjan Mishra <rajeevranjanmishra4339@gmail.com>
2026-06-30 23:39:22 +0530

    LC136SingleNumber

---
+++ LeetCodeSolutions/src/LC136SingleNumber.java
@@ -0,0 +1,15 @@
+public class LC136SingleNumber {
+    public static void main(String[] args) {
+        int[] arr = {4, 1, 2, 1, 2};
+        int ans = singleNumber(arr);
+        System.out.printf(String.valueOf(ans));
+    }
+
+    public static int singleNumber(int[] nums) {
+        int unique = 0;
+        for (int n : nums) {
+            unique ^= n;
+        }
+        return unique;
+    }
+}
