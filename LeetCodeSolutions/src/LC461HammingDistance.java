4f377924de61e398a729bb7a67c3caabb26d4605
Rajeev Ranjan Mishra <rajeevranjanmishra4339@gmail.com>
2026-07-04 23:31:54 +0530

    code

--- LeetCodeSolutions/src/LC461HammingDistance.java
+++ LeetCodeSolutions/src/LC461HammingDistance.java
@@ -1,8 +1,8 @@
-public class LC461 {
+public class LC461HammingDistance {
     // https://leetcode.com/problems/hamming-distance/
 
     // The Hamming distance between two integers is the number of positions at which the corresponding bits are different.
-    //Given two integers x and y, return the Hamming distance between them.
+    //Given two integers x and y, return the Hamming distance between them. 0 <= x, y <= 231 - 1
     public static void main(String[] args) {
         int x = 1, y = 4;
         System.out.println(hammingDistance(x, y));
