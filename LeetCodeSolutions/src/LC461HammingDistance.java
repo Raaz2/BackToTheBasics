7b01a0b014e17ccb6eb92f81128357b07ee6b7bc
Rajeev Ranjan Mishra <rajeevranjanmishra4339@gmail.com>
2026-07-05 23:05:48 +0530

    code

--- LeetCodeSolutions/src/LC461HammingDistance.java
+++ LeetCodeSolutions/src/LC461HammingDistance.java
@@ -5,9 +5,19 @@ public class LC461HammingDistance {
     //Given two integers x and y, return the Hamming distance between them. 0 <= x, y <= 231 - 1
     public static void main(String[] args) {
         int x = 1, y = 4;
+
         System.out.println(hammingDistance(x, y));
     }
     public static int hammingDistance(int x, int y) {
-        return Integer.bitCount(x ^ y);
+        int n = x ^ y;
+        System.out.println("Decimal => " + n);
+        System.out.println("Binary => " + Integer.toBinaryString(n));
+        int count = 0;
+        while (n > 0) {
+            count++;
+            n -= n & -n;
+        }
+        return count;
+//        return Integer.bitCount(x ^ y);
     }
 }
---
+++ LeetCodeSolutions/src/LC832FlipImage.java
@@ -0,0 +1,33 @@
+public class LC832FlipImage {
+
+    public static void main(String[] args) {
+        int[][] image = {{1,1,0},{1,0,1},{0,0,0}};
+        for (int[] row : image) {
+            for (int i : row) {
+                System.out.print(i + ", ");
+            }
+            System.out.println();
+        }
+        System.out.println("=====================================");
+        image = flipAndInvertImage(image);
+        for (int[] row : image) {
+            for (int i : row) {
+                System.out.print(i + ", ");
+            }
+            System.out.println();
+        }
+    }
+
+
+    public static int[][] flipAndInvertImage(int[][] image) {
+        for (int[] row : image) {
+            for (int i = 0; i < (row.length + 1) / 2; i++) {
+                //swap
+                int temp = row[i] ^ 1;
+                row[i] = row[row.length - i - 1] ^ 1;
+                row[row.length - i - 1] = temp;
+            }
+        }
+        return image;
+    }
+}
