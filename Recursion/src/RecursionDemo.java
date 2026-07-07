bc98bc947f41a1a2a7752bef3db94634a61e2ec1
Rajeev Ranjan Mishra <rajeevranjanmishra4339@gmail.com>
2026-07-07 23:15:20 +0530

    Recursion

--- Recursion/src/RecursionDemo.java
+++ Recursion/src/RecursionDemo.java
@@ -1,6 +1,7 @@
 public class RecursionDemo {
     public static void main(String[] args) {
-        print(1);
+//        print(1);
+        printRev(5);
     }
     public static void print(int i) {
         if (i >= 6) {
@@ -9,5 +10,12 @@ public class RecursionDemo {
         System.out.println(i);
         i++;
         print(i);
+    }
+    public static void printRev(int i) {
+        if (i <= 0) {
+            return;
+        }
+        System.out.println(i);
+        printRev(i - 1);
     }
 }
