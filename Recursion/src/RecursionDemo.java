e8a9e48b7f2294d5f896ecef7a98e15113ef7d66
Rajeev Ranjan Mishra <rajeevranjanmishra4339@gmail.com>
2026-07-08 23:34:18 +0530

    sumOfDigits

--- Recursion/src/RecursionDemo.java
+++ Recursion/src/RecursionDemo.java
@@ -2,12 +2,17 @@ public class RecursionDemo {
     public static void main(String[] args) {
 //        print(1);
         printRev(5);
+       int ans =  factorial(6);
+        System.out.println(ans);
+
+
+        System.out.println(sumOfDigits(1578));
     }
     public static void print(int i) {
         if (i >= 6) {
             return;
         }
-        System.out.println(i);
+        System.out.print(i + " ");
         i++;
         print(i);
     }
@@ -17,5 +22,27 @@ public class RecursionDemo {
         }
         System.out.println(i);
         printRev(i - 1);
+    }
+
+    public static int factorial(int i) {
+        if (i <= 1) {
+            return 1;
+        }
+        return i * factorial(i - 1);
+    }
+//    public static int sumOfDigits(int n) {
+//        if (n <= 1) {
+//            return n;
+//        }
+//
+//        return 1 + () n % 10
+//    }
+    public static int sumOfDigits(int n) {
+        int res = 0;
+        while (n > 0) {
+            res += n % 10;
+            n /= 10;
+        }
+        return res;
     }
 }
