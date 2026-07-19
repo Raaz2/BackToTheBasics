ab695c4ecf74abdafcadb1aa5c801113d03ea06f
Rajeev Ranjan Mishra <rajeevranjanmishra4339@gmail.com>
2026-07-19 22:48:19 +0530

    LC509FibDP

---
+++ LeetCodeSolutions/src/LC509FibDP.java
@@ -0,0 +1,19 @@
+public class LC509FibDP {
+    public static void main(String[] args) {
+        int n = 60;
+        int ans = fib(n, new int[n + 1]);
+
+        System.out.println(ans);
+    }
+
+    public static int fib(int n, int[] dp) {
+        if (n <= 1) return n;
+
+        if (dp[n] != 0) {
+            return dp[n];
+        }
+        else {
+            return dp[n] = fib(n - 1, dp) + fib(n - 2, dp);
+        }
+    }
+}
