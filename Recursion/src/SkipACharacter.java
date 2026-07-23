4a4d6452401fbc751fc4379808ca7ac458252812
Rajeev Ranjan Mishra <rajeevranjanmishra4339@gmail.com>
2026-07-23 23:51:11 +0530

    SkipWithReturn

--- Recursion/src/SkipACharacter.java
+++ Recursion/src/SkipACharacter.java
@@ -3,6 +3,9 @@ public class SkipACharacter {
         String str = "abbccdaaefa";
 
         removeCharRecursive(str, "");
+
+        String ans = removeCharRecursiveWithReturn(str);
+        System.out.println("Returned ans without 'A' is " + ans);
     }
 
     public static void removeCharRecursive(String str, String ans) {
@@ -21,6 +24,24 @@ public class SkipACharacter {
         // If it is not 'a', add it to our answer accumulator
         else {
             removeCharRecursive(str.substring(1), ans + ch);
+        }
+    }
+
+    public static String  removeCharRecursiveWithReturn(String str) {
+        if (str.isEmpty()) {
+            return "";
+        }
+
+        // Take the first character of the remaining string
+        char ch = str.charAt(0);
+
+        // If it is 'a', skip it and process the rest of the string
+        if (ch == 'a') {
+            return removeCharRecursiveWithReturn(str.substring(1));
+        }
+        // If it is not 'a', add it to our answer accumulator
+        else {
+            return ch + removeCharRecursiveWithReturn(str.substring(1));
         }
     }
 }
