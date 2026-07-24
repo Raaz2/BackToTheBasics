556282a839f29d1d13fdc2abf1f9690336249ace
Rajeev Ranjan Mishra <rajeevranjanmishra4339@gmail.com>
2026-07-24 23:47:07 +0530

    subsets

--- Recursion/src/SubSequence.java
+++ Recursion/src/SubSequence.java
@@ -1,7 +1,14 @@
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.List;
+
 public class SubSequence {
     public static void main(String[] args) {
         String str = "abcd";
         subseq(str, "");
+        System.out.println("================================");
+        List<String> list = subseqlistreturn(str, "");
+        System.out.println(list);
     }
     public static void subseq(String str, String ans) {
 
@@ -14,5 +21,23 @@ public class SubSequence {
 
         subseq( str.substring(1),ans + ch);
         subseq(str.substring(1), ans);
+    }
+
+    public static ArrayList<String> subseqlistreturn(String str, String ans) {
+
+        if (str.isEmpty()) {
+            ArrayList<String> list = new ArrayList<>();
+            list.add(ans);
+//            System.out.println(ans);
+            return list;
+        }
+
+        char ch = str.charAt(0);
+
+        ArrayList<String> selected = subseqlistreturn( str.substring(1),ans + ch);
+        ArrayList<String> rejected = subseqlistreturn(str.substring(1), ans);
+
+        selected.addAll(rejected);
+        return selected;
     }
 }
