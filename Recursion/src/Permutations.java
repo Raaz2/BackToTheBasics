e54d2ea056fd277e80d35dc91dc05d9b40725ab8
Rajeev Ranjan Mishra <rajeevranjanmishra4339@gmail.com>
2026-07-25 23:57:23 +0530

    code

--- Recursion/src/Permutations.java
+++ Recursion/src/Permutations.java
@@ -1,6 +1,11 @@
+import java.util.ArrayList;
+
 public class Permutations {
     public static void main(String[] args) {
         permutations("", "abc");
+        ArrayList<String> list = permutationsReturn("", "xyz");
+        System.out.println("==============================");
+        System.out.println(list);
     }
 
     public static void permutations(String p, String up) {
@@ -16,5 +21,24 @@ public class Permutations {
             String s = p.substring(i, p.length());
             permutations(f + ch + s, up.substring(1));
         }
+    }
+
+
+    public static ArrayList<String> permutationsReturn(String p, String up) {
+        ArrayList<String> list = new ArrayList<>();
+        if (up.isEmpty()) {
+            list.add(p);
+            return list;
+        }
+
+        char ch = up.charAt(0);
+
+        for (int i = 0; i <= p.length(); i++) {
+            String f = p.substring(0, i);
+            String s = p.substring(i, p.length());
+            list.addAll(permutationsReturn(f + ch + s, up.substring(1)));
+        }
+
+        return list;
     }
 }
