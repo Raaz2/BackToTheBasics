97e0b0eac95d69f7937659276dd424f639257c3b
Rajeev Ranjan Mishra <rajeevranjanmishra4339@gmail.com>
2026-08-08 21:18:59 +0530

    streak

--- Sorting/src/BubbleSort.java
+++ Sorting/src/BubbleSort.java
@@ -2,7 +2,7 @@ import java.util.Arrays;
 
 public class BubbleSort {
     public static void main(String[] args) {
-        int[] arr = {1, 3, 4, 2, 5, 0};
+        int[] arr = {1, 3, 4, 2, 5, 99};
         System.out.println(Arrays.toString(arr));
         for (int i = 0; i < arr.length; i++) { // at every pass maximum element comes to the end
             for (int j = 0; j < arr.length - i - 1; j++) { // because ith from end already at it's correct place
