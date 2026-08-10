47038340f2ece5e66cc83542044b375770ca8840
Rajeev Ranjan Mishra <rajeevranjanmishra4339@gmail.com>
2026-08-10 23:06:36 +0530

    code

--- Sorting/src/BubbleSort.java
+++ Sorting/src/BubbleSort.java
@@ -2,7 +2,7 @@ import java.util.Arrays;
 
 public class BubbleSort {
     public static void main(String[] args) {
-        int[] arr = {87, 3, 49, 2, 5, 99};
+        int[] arr = {87, 3, 49, 12, 5, 99};
         System.out.println(Arrays.toString(arr));
         for (int i = 0; i < arr.length; i++) { // at every pass maximum element comes to the end
             for (int j = 0; j < arr.length - i - 1; j++) { // because ith from end already at it's correct place
