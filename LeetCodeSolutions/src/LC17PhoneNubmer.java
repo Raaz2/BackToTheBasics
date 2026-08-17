fc3f2e8552a7764e9a041feae979386bafe5d329
Rajeev Ranjan Mishra <rajeevranjanmishra4339@gmail.com>
2026-08-17 23:47:16 +0530

    code

--- LeetCodeSolutions/src/LC17PhoneNubmer.java
+++ LeetCodeSolutions/src/LC17PhoneNubmer.java
@@ -5,7 +5,7 @@ public class LC17PhoneNubmer {
     public static void main(String[] args) {
         Solution solution = new LC17PhoneNubmer.Solution();
 
-        String digits = "59";
+        String digits = "95";
         List<String> ans = solution.letterCombinations(digits);
         System.out.println(ans);
 
