d114381a90b10c8ad7b1d882295ea6d05e5ac485
Rajeev Ranjan Mishra <rajeevranjanmishra4339@gmail.com>
2026-08-26 22:22:32 +0530

    code

--- Test.java
+++ Test.java
@@ -3,5 +3,7 @@ public class Test {
         System.out.println("Hello World! This is the testing program of the BackToTheBasics repo after a fresh pull!");
         System.out.println("Going to Ujjain!!! Mahakaleshwar");
         System.out.println("Hopefully today we I can start the much awaited project!");
+        System.out.println("Hello World!");
+
     }
 }
