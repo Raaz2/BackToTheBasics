c59f2b9fd5ade6f053fc79d652dd5fef6ecc63b8
Rajeev Ranjan Mishra <112801262+Raaz2@users.noreply.github.com>
2026-09-10 23:56:17 +0530

    Update Test.java

--- Test.java
+++ Test.java
@@ -3,8 +3,6 @@ public class Test {
         System.out.println("Hello World! This is the testing program of the BackToTheBasics repo after a fresh pull!");
         System.out.println("Hopefully today we I can start the much awaited project!");
         System.out.println("Hello World!");
-        System.out.println("Hello World!");
-        System.out.println("Hello World!");
         System.out.println("Remembering that I've created a repo where I've to be consistent");
     }
 }
